7f7a62637e76f5a63c8ee071e510d35472c1441e
Diogo Redin <diogo.redin@gmail.com>
2017-10-26 17:26:10 +0100

    Class Example

---
+++ proj.java
@@ -0,0 +1,10 @@
+/* Class 26/10 */
+p.DoShowNumberServices(TicketOffice to) {
+    super("Numero de Serviços", to);
+}
+
+@Override
+public void execture() {
+    int n = receiver.getServices().size();
+}
+
